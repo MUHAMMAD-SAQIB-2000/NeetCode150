d316f4f88401aa5e7eb28b87ffae7810793248f0
MUHAMMAD-SAQIB-2000 <saqibmakhan@gmail.com>
2026-07-25 17:11:03 +0500

    updated class of ReverseLinkedList

--- src/easyproblems/ReverseLinkedList.java
+++ src/easyproblems/ReverseLinkedList.java
@@ -1,23 +1,23 @@
 package easyproblems;
 
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode() {
-    }
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}
-
 public class ReverseLinkedList {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
 
     public static void main(String[] args) {
         ListNode list1 = buildList(new int[]{1, 2, 3, 4});
